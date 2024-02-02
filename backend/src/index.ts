c899e927b845782b3c9c055b1774d3e844032223
Naveen0910 <125568964+Naveen0910@users.noreply.github.com>
2024-02-02 15:52:14 +0530

    User registration using express-validator , jwt , httponly cookie

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Response, Request } from "express";
 import cors from "cors";
 import "dotenv/config"; //loading env variables
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -11,10 +12,13 @@ app.use(express.json()); //convert body in api req to json automatically for us
 app.use(express.urlencoded({ extended: true })); // helps us in parsing URL
 app.use(cors()); //enables allows cross origin resource sharing
 
-// example
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello from express - ts" });
-});
+// integreating routes with server instance
+app.use("/api/users", userRoutes);
+
+// // example
+// app.get("/api/test", async (req: Request, res: Response) => {
+//   res.json({ message: "Hello from express - ts" });
+// });
 
 // making app listen to a port
 app.listen(7000, () => {
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,3 +1,20 @@
 import express from "express";
-
+import { userRegistration } from "../controller/users";
+import { check } from "express-validator";
 const router = express.Router();
+
+/*   /api/users/register                                   */
+router.post(
+  "/register",
+  [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  userRegistration
+);
+
+export default router;
